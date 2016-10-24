55bb524ffacdb0ff78c8697159856f0867e53385
chelsea lin <clin5@cca.edu>
2016-10-24 11:56:11 -0700

    edit

---
+++ projects/project 2/js/script.js
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>jQuery UI Effects - Easing demo</title>
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+  <style>
+  .graph {
+    float: left;
+    margin-left: 10px;
+  }
+  </style>
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script>
+  $(dc).keypress(function(key){
+    if key=="Enter"
+      Playmusic();
+  }
+  Def Playmusic(){
+    $
+    ('#yourMusic Player').play()
+  }
+   
+  </script>
+ 
