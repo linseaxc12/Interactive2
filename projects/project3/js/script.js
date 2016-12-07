b7c2734276e2b3f1bf4c4ee23085b7ae69615946
chelsea lin <clin5@cca.edu>
2016-12-07 12:05:06 -0800

    edit

--- projects/project3/js/script.js
+++ projects/project3/js/script.js
@@ -1,10 +1,134 @@
 $(document).ready(function(){
-	
+	// alert('hi');
+
 	$topIndex = 0;
 
-	&( "ui-draggable").draggable().css({"position"})
+	$(".ui-draggable").draggable();
 
-	$("ui-draggable" click (function)){
-		#topIndex = $topindex 
+	$(".ui-draggable").click (function(){
+		$topIndex = $topIndex+1; 
 		$(this).css("z-index",$topIndex);
-	}
+	});
+	$(".ui-draggable1").draggable();
+
+	$(".ui-draggable1").click (function(){
+		$topIndex = $topIndex+2; 
+		$(this).css("z-index",$topIndex);
+	});
+	$(".ui-draggable2").draggable();
+
+	$(".ui-draggable2").click (function(){
+		$topIndex = $topIndex+3; 
+		$(this).css("z-index",$topIndex);
+	});
+	$(".ui-draggable3").draggable();
+
+	$(".ui-draggable3").click (function(){
+		$topIndex = $topIndex+1; 
+		$(this).css("z-index",$topIndex);
+	});
+	$(".ui-draggable4").draggable();
+
+	$(".ui-draggable4").click (function(){
+		$topIndex = $topIndex+2; 
+		$(this).css("z-index",$topIndex);
+	});
+	$(".ui-draggable5").draggable();
+
+	$(".ui-draggable5").click (function(){
+		$topIndex = $topIndex+3; 
+		$(this).css("z-index",$topIndex);
+	});
+	$(".ui-draggable6").draggable();
+
+	$(".ui-draggable6").click (function(){
+		$topIndex = $topIndex+2; 
+		$(this).css("z-index",$topIndex);
+	});
+	$(".ui-draggable7").draggable();
+
+	$(".ui-draggable7").click (function(){
+		$topIndex = $topIndex+3; 
+		$(this).css("z-index",$topIndex);
+	});
+	$(".ui-draggable8").draggable();
+
+	$(".ui-draggable8").click (function(){
+		$topIndex = $topIndex+1; 
+		$(this).css("z-index",$topIndex);
+	});
+	$(".ui-draggable9").draggable();
+
+	$(".ui-draggable9").click (function(){
+		$topIndex = $topIndex+2; 
+		$(this).css("z-index",$topIndex);
+	});
+	$(".ui-draggable10").draggable();
+
+	$(".ui-draggable10").click (function(){
+		$topIndex = $topIndex+3; 
+		$(this).css("z-index",$topIndex);
+	});
+	$(".ui-draggable11").draggable();
+
+	$(".ui-draggable11").click (function(){
+		$topIndex = $topIndex+3; 
+		$(this).css("z-index",$topIndex);
+	});
+	$(".ui-draggable12").draggable();
+
+	$(".ui-draggable12").click (function(){
+		$topIndex = $topIndex+3; 
+		$(this).css("z-index",$topIndex);
+	});
+	$(".ui-draggable13").draggable();
+
+	$(".ui-draggable13").click (function(){
+		$topIndex = $topIndex+1; 
+		$(this).css("z-index",$topIndex);
+	});
+	$(".ui-draggable14").draggable();
+
+	$(".ui-draggable14").click (function(){
+		$topIndex = $topIndex+2; 
+		$(this).css("z-index",$topIndex);
+	});
+	$(".ui-draggable15").draggable();
+
+	$(".ui-draggable15").click (function(){
+		$topIndex = $topIndex+3; 
+		$(this).css("z-index",$topIndex);
+	});
+	$(".ui-draggable16").draggable();
+
+	$(".ui-draggable16").click (function(){
+		$topIndex = $topIndex+2; 
+		$(this).css("z-index",$topIndex);
+	});
+	$(".ui-draggable17").draggable();
+
+	$(".ui-draggable17").click (function(){
+		$topIndex = $topIndex+3; 
+		$(this).css("z-index",$topIndex);
+	});
+	$(".ui-draggable18").draggable();
+
+	$(".ui-draggable18").click (function(){
+		$topIndex = $topIndex+1; 
+		$(this).css("z-index",$topIndex);
+	});
+	$(".ui-draggable19").draggable();
+
+	$(".ui-draggable19").click (function(){
+		$topIndex = $topIndex+2; 
+		$(this).css("z-index",$topIndex);
+	});
+	$(".ui-draggable20").draggable();
+
+	$(".ui-draggable20").click (function(){
+		$topIndex = $topIndex+3; 
+		$(this).css("z-index",$topIndex);
+	});
+});
+
+	
